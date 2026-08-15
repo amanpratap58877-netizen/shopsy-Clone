8701142a6a295a29ee092da0e88b19d61c5a3724
Aman pratap singh <amanpratap58877@gmail.com>
2026-08-16 00:24:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
     try{
     const res= await fetch('https://dummyjson.com/products/search?q=phone');
     const data =await res.json();
-    console.log(data.products);
+    // console.log(data.products);
     const heroContainer=document.querySelector('.hero');
       data.products.forEach(element => {
       heroContainer.innerHTML+=
   `<div class="cards">
-    <img id="card-img" src="${element.thumbnail
+    <img class="card-img" src="${element.thumbnail
     }" alt="">
     <h1>${element.title}
     </h1>
-    <p id="price">₹${element.price}</p>
-    <button id="cart-btn">Add to cart</button>
+    <p class="price">₹${element.price}</p>
+    <button class="cart-btn">Add to cart</button>
   </div>`
     });
   }
@@ -32,12 +32,12 @@
       data.products.forEach(element => {
       heroContainer.innerHTML+=
   `<div class="cards">
-    <img id="card-img" src="${element.thumbnail
+    <img class="card-img" src="${element.thumbnail
     }" alt="">
     <h1>${element.title}
     </h1>
     <p id="price">₹${element.price}</p>
-    <button id="cart-btn">Add to cart</button>
+    <button class="cart-btn">Add to cart</button>
   </div>`
     });
   }
@@ -46,3 +46,41 @@
   }
   }
   MakeupPdt();
+
+
+  const show=document.querySelectorAll(".sub-container,.aside-container,.cat-btn,.choice");
+  const buton=document.querySelector("#mode");
+  const bdy=document.querySelector("body");
+  const top1=document.querySelectorAll("header,.nav");
+
+  const change=()=>{
+  show.forEach(element=>{
+    element.classList.toggle("theme");
+  })
+    buton.style.border="1px solid blue";
+    if(show[0].classList.contains("theme")){
+      buton.innerHTML=`<i class="fa-solid fa-sun"></i>`;
+        bdy.style.background = "linear-gradient(to right, orange, white, green)";
+        top1.forEach(ele=>{
+          ele.style.color="#ffffff";
+        })
+    }
+    else{
+       buton.innerHTML=`<i class="fa-regular fa-moon"></i>`
+        bdy.style.backgroundColor="#ffffff"
+         top1.forEach(ele=>{
+          ele.style.color="#000000";
+        })
+    }
+     
+    
+  }
+
+  buton.addEventListener("click",change);
+
+
+
+
+
+
+  
